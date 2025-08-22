f0022dc48a2ca47ddcd3923f898a64978daf17e5
srivasanth23 <jammulasrivasanth@gmial.com>
2025-08-22 10:59:28 +0530

    Add project files.

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Conetent { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+        // It represents a collection navigation property in Entity Framework.
+        // Used to define a one-to-many or many-to-many relationship.
+        // Tells EF Core that "one entity can have many related entities".
+    }
+}
